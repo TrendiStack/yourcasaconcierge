13609c8852a96857805425991152bfc7d9e803f1
UziStacks <terel.phillips23@gmail.com>
2022-11-15 12:53:45 -0500

    -made menu text visible on smaller mobile devices

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,3 +1,4 @@
+import content from "../content";
 import { useContext } from "react";
 import { MenuContext } from "../context/MenuContext";
 import { HashLink as Link } from "react-router-hash-link";
@@ -12,6 +13,7 @@ import { motion as m } from "framer-motion";
 
 const Menu = () => {
   const { isOpen, toggleMenu } = useContext(MenuContext);
+  const { nav } = content;
   return (
     <>
       <m.div
@@ -20,34 +22,18 @@ const Menu = () => {
         transition={{ duration: 0.5 }}
         className={`
         fixed top-0 left-0 h-screen w-4/5 bg-white z-40 shadow-lg
-    lg:hidden bg-variant garamond text-white text-4xl py-28 px-8 `}
+    lg:hidden bg-variant garamond text-white menu-text py-28 px-8 `}
       >
         <ul className="flex flex-col gap-10">
-          <li>
-            <Link onClick={toggleMenu} to="#about">
-              About
-            </Link>
-          </li>
-          <li>
-            <Link onClick={toggleMenu} to="#services">
-              Services
-            </Link>
-          </li>
-          <li>
-            <Link onClick={toggleMenu} to="#prices">
-              Pricing
-            </Link>
-          </li>
-          <li>
-            <Link onClick={toggleMenu} to="#brands">
-              Brands
-            </Link>
-          </li>
-          <li>
-            <Link onClick={toggleMenu} to="#contact">
-              Contact
-            </Link>
-          </li>
+          {nav.map((item) => {
+            return (
+              <li key={item.id}>
+                <Link onClick={toggleMenu} to={`/#${item.path}`}>
+                  {item.title}
+                </Link>
+              </li>
+            );
+          })}
           <div>
             <p className="text-2xl">1 800 123 4567</p>
             <p className="text-xl ">hello@youcasaconcierge.com</p>
