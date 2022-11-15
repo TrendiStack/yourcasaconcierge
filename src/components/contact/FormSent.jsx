2906d49d9afcdee2abdcce2d1477242d5a5f5476
UziStacks <terel.phillips23@gmail.com>
2022-11-15 13:53:23 -0500

    -styled the success page

--- src/components/contact/FormSent.jsx
+++ src/components/contact/FormSent.jsx
@@ -1,22 +1,44 @@
 import { motion as m } from "framer-motion";
-import { Link } from "react-router-dom";
+import waves from "../../assets/backgrounds/wave_background.svg";
 
 const FormSent = () => {
   return (
-    <m.div
+    <div
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       exit={{ opacity: 0 }}
       transition={{ duration: 0.5 }}
-      className="flex flex-col items-center justify-center w-full h-screen bg-varaint "
+      className="flex flex-col items-center justify-center w-full h-screen bg-varaint garamond  header-text-variant"
     >
-      <h1>Thank you for your message!</h1>
-      <p>We will get back to you as soon as possible.</p>
+      <img
+        src={waves}
+        alt="waves"
+        className="absolute top-0 left-0 w-full h-full object-cover z-[-1]"
+      />
+      <m.div
+        initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5 }}
+        className="text-center text-white"
+      >
+        <h1>Thank you for your message!</h1>
+        <p>We will get back to you as soon as possible.</p>
+      </m.div>
 
-      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-        <Link to="/">Go back to home</Link>
-      </button>
-    </m.div>
+      <m.button
+        initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5, delay: 0.5 }}
+        className=""
+      >
+        <a
+          href="/"
+          className="text-xl xl:text-3xl text-white xl:hover:bg-black transition-all duration-300 ease-in-out xl:hover:p-3"
+        >
+          GO BACK HOME
+        </a>
+      </m.button>
+    </div>
   );
 };
 
