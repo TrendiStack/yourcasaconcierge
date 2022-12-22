b6a8dc5b09b0fbb94abae4ccd6639c6ca83b1c42
Terel <terel.phillips23@gmail.com>
2022-12-22 14:21:27 -0500

    -h3 to h2

--- src/components/about/AboutArticle.jsx
+++ src/components/about/AboutArticle.jsx
@@ -1,9 +1,9 @@
 const AboutArticle = ({ title, text }) => {
   return (
     <article className="bg-dark p-5">
-      <h3 className="text-secondary text-2xl xl:text-3xl garamond mb-2">
+      <h2 className="text-secondary text-2xl xl:text-3xl garamond mb-2">
         {title}
-      </h3>
+      </h2>
       <p className="text-light">{text}</p>
     </article>
   );
