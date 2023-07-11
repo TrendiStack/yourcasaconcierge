e1ee8ce31aaa0061e778e0f9de6a47007a497887
Terel <terel.phillips23@gmail.com>
2023-07-11 07:43:08 -0400

    -removed plugin

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,5 +1,3 @@
-const plugin = require('tailwindcss/plugin');
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
@@ -13,27 +11,6 @@ module.exports = {
         laptopl: '1440px',
         desktopl: '1440px',
       },
-      textShadow: {
-        sm: '0 1px 2px var(--tw-shadow-color)',
-        DEFAULT: '0 2px 4px var(--tw-shadow-color)',
-        lg: '0 8px 16px var(--tw-shadow-color)',
-      },
     },
   },
-  plugins: [
-    plugin(function ({ addUtilities, theme }) {
-      matchUtilities(
-        {
-          'text-shadow': value => {
-            return {
-              '--tw-text-shadow': value,
-            };
-          },
-        },
-        {
-          values: theme('textShadow'),
-        }
-      );
-    }),
-  ],
 };
