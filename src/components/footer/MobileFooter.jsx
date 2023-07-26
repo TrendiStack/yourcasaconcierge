6870e2e6fd008752ddc6060353902d6a0954451d
Terel <terel.phillips23@gmail.com>
2023-07-26 13:55:01 -0400

    -testing something

--- src/components/footer/MobileFooter.jsx
+++ src/components/footer/MobileFooter.jsx
@@ -14,6 +14,7 @@ const MobileFooter = () => {
   const footer = foot[0] === undefined ? null : foot[0];
   return (
     <div className="container xl:hidden layout-padding py-10">
+      test
       <LargeLogo bg="bg-light" />
       <div className="flex justify-between my-4">
         <ul className="flex flex-col gap-2">
